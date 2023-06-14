0a346bf3194498f25c3240b3c5e9a6a3f26085b9
marcosgwt0 <marcoswsz0503@gmail.com>
2023-06-14 18:18:49 -0300

    melhorias no codigo e output novo

--- PjBL.java
+++ PjBL.java
@@ -8,8 +8,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.PrintWriter;
+
 
 abstract class Conta {
     protected String mes;
@@ -23,6 +23,11 @@ abstract class Conta {
     }
 
     public abstract void exibirConta();
+
+    @Override
+    public String toString() {
+        return "Conta [mes=" + mes + ", consumo=" + consumo + ", custo=" + custo + "]";
+    }
 }
 
 class ContaEnergia extends Conta {
@@ -31,10 +36,15 @@ class ContaEnergia extends Conta {
     }
 
     public void exibirConta() {
-        System.out.println("Conta de Energia para o mês de " + mes);
+        System.out.println("Conta de Energia - " + mes);
         System.out.println("Consumo: " + consumo + " kWh");
         System.out.println("Custo: R$" + custo);
-        return;
+        System.out.println();
+    }
+
+    @Override
+    public String toString() {
+        return "ContaEnergia [mes=" + mes + ", consumo=" + consumo + ", custo=" + custo + "]";
     }
 }
 
@@ -44,12 +54,18 @@ class ContaAgua extends Conta {
     }
 
     public void exibirConta() {
-        System.out.println("Conta de Água para o mês de " + mes);
-        System.out.println("Consumo: " + consumo + " metros cúbicos");
+        System.out.println("Conta de Água - " + mes);
+        System.out.println("Consumo: " + consumo + " m³");
         System.out.println("Custo: R$" + custo);
-        return;
+        System.out.println();
+    }
+
+    @Override
+    public String toString() {
+        return "ContaAgua [mes=" + mes + ", consumo=" + consumo + ", custo=" + custo + "]";
     }
 }
+
 
 class ComparadorContas {
     private List<Conta> contas;
@@ -65,15 +81,20 @@ class ComparadorContas {
         message = new StringBuilder();
     }
 
+    class FormatoInvalidoException extends Exception {
+        public FormatoInvalidoException(String mensagem) {
+            super(mensagem);
+        }
+    }
+
     public void carregarContas(String nomeArquivo) {
         try (BufferedReader leitor = new BufferedReader(new FileReader(nomeArquivo))) {
             String linha;
             while ((linha = leitor.readLine()) != null) {
                 String[] partes = linha.split(",");
-                if (partes.length != 4) {
-                    System.out.println("Formato de linha inválido: " + linha);
-                    continue;
-                }
+            if (partes.length != 4) {
+                throw new FormatoInvalidoException("Formato de linha inválido: " + linha);
+            }
                 String tipoConta = partes[0];
                 String mes = partes[1];
                 double consumo = Double.parseDouble(partes[2]);
@@ -87,8 +108,10 @@ class ComparadorContas {
             }
             System.out.println("Contas carregadas com sucesso.");
             calcularMediaConsumo();
-        } catch (IOException | NumberFormatException e) {
+         } catch (IOException | NumberFormatException e) {
             System.out.println("Erro ao carregar as contas: " + e.getMessage());
+        } catch (FormatoInvalidoException e) {
+            System.out.println("Erro de formato: " + e.getMessage());
         }
     }
 
@@ -113,8 +136,22 @@ class ComparadorContas {
             comparacoes.add(resultado);
         }
 
-        lastConsumo = contas.get(contas.size() - 1).consumo;
-        lastCusto = contas.get(contas.size() - 1).custo;
+        // Compare the last account with the previous one
+        Conta lastConta = contas.get(contas.size() - 1);
+        Conta previousConta = contas.get(contas.size() - 2);
+
+        String[] resultado = new String[6];
+        resultado[0] = previousConta.mes;
+        resultado[1] = lastConta.mes;
+        resultado[2] = Double.toString(previousConta.consumo);
+        resultado[3] = Double.toString(lastConta.consumo);
+        resultado[4] = Double.toString(previousConta.custo);
+        resultado[5] = Double.toString(lastConta.custo);
+
+        comparacoes.add(resultado);
+
+        lastConsumo = lastConta.consumo;
+        lastCusto = lastConta.custo;
     }
 
     private void calcularMediaConsumo() {
@@ -221,8 +258,23 @@ class ComparadorContas {
         });
         panel.add(buttonAgua);
 
+        JButton buttonCalculator = new JButton("Calculadora");
+        buttonCalculator.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            showCalculator();
+        }
+    });
+    panel.add(buttonCalculator);
+
         return panel;
     }
+
+    private void showCalculator() {
+    CalculatorExtension calculator = new CalculatorExtension();
+    JPanel calculatorPanel = calculator.getCalculatorPanel();
+    mainPanel.add(calculatorPanel, "calculator");
+    cardLayout.show(mainPanel, "calculator");
+}
 
     private void carregarContas(String tipoConta) {
         int returnValue = fileChooser.showOpenDialog(frame);
@@ -240,14 +292,11 @@ class ComparadorContas {
         // First Table
         String[] columnNames = {"Mês Anterior", "Mês Atual", "Consumo Anterior (KWh ou m3)", "Consumo Atual (KWh ou m3)", "Custo Anterior", "Custo Atual"};
         tableModel = new DefaultTableModel(columnNames, 0);
-
         tableModel.setRowCount(0);
-
         List<String[]> comparacoes = comparadorContas.getComparacoes();
         for (String[] comparacao : comparacoes) {
             tableModel.addRow(comparacao);
         }
-
         JTable table = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(table);
         panel.add(scrollPane, BorderLayout.PAGE_START);
@@ -255,7 +304,6 @@ class ComparadorContas {
         // Second Table
         String[] columnNames2 = {"Custo Médio", "Consumo Médio", "Consumo Mínimo (KWh ou m3)", "Consumo Máximo (KWh ou m3)", "Custo Mínimo", "Custo Máximo"};
         tableModel2 = new DefaultTableModel(columnNames2, 0);
-
         String[] row = new String[6];
 
         double averageCost = comparadorContas.getComparacoes().stream()
@@ -299,30 +347,31 @@ class ComparadorContas {
         JTable table2 = new JTable(tableModel2);
         JScrollPane scrollPane2 = new JScrollPane(table2);
 
+        // Message Box
         JTextArea messageArea = new JTextArea(comparadorContas.getMessage());
         messageArea.setEditable(false);
         messageArea.setLineWrap(true);
         messageArea.setWrapStyleWord(true);
         JScrollPane messageScrollPane = new JScrollPane(messageArea);
+        panel.add(messageScrollPane, BorderLayout.CENTER);
 
-        // Panel for the message box
-        JPanel innerPanel = new JPanel();
-        innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS));
-        innerPanel.add(scrollPane2);
-        innerPanel.add(messageScrollPane);
+        // Create a panel for the tables and message box
+        JPanel tablesPanel = new JPanel(new GridLayout(3, 1));
+        tablesPanel.add(scrollPane);
+        tablesPanel.add(scrollPane2);
+        tablesPanel.add(messageScrollPane);
+        panel.add(tablesPanel, BorderLayout.CENTER);
 
-        panel.add(innerPanel, BorderLayout.CENTER);
-
-        // Create a button to save the table data to a file
+        // Create a panel for the buttons
+        JPanel buttonsPanel = new JPanel(new FlowLayout());
         JButton saveButton = new JButton("Salvar");
         saveButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 saveTableData();
             }
         });
-        panel.add(saveButton, BorderLayout.PAGE_END);
+        buttonsPanel.add(saveButton);
 
-        // Create a button to go back to the menu
         JButton backButton = new JButton("Voltar ao Menu");
         backButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -331,40 +380,41 @@ class ComparadorContas {
                 cardLayout.show(mainPanel, "menu");
             }
         });
-        panel.add(backButton, BorderLayout.PAGE_END);
+        buttonsPanel.add(backButton);
+
+        panel.add(buttonsPanel, BorderLayout.PAGE_END);
 
         mainPanel.add(panel, tipoConta);
         cardLayout.show(mainPanel, tipoConta);
+
+        
     }
 
     private void saveTableData() {
         int returnValue = fileChooser.showSaveDialog(frame);
         if (returnValue == JFileChooser.APPROVE_OPTION) {
             String nomeArquivo = fileChooser.getSelectedFile().getPath();
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
-                // Write the data from the first table
+            try (PrintWriter writer = new PrintWriter(nomeArquivo)) {
+                writer.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                writer.println("Mês Anterior     Mês Atual        Consumo Anterior Consumo Atual   Custo Anterior   Custo Atual");
+                writer.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
                 for (int i = 0; i < tableModel.getRowCount(); i++) {
                     for (int j = 0; j < tableModel.getColumnCount(); j++) {
-                        writer.write(tableModel.getValueAt(i, j).toString());
-                        if (j < tableModel.getColumnCount() - 1) {
-                            writer.write(",");
-                        }
+                        String value = tableModel.getValueAt(i, j).toString();
+                        writer.printf("%-17s", value);
                     }
-                    writer.newLine();
+                    writer.println();
                 }
 
-                // Add an empty line to separate the tables
-                writer.newLine();
-
-                // Write the data from the second table
+                writer.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                writer.println("custo médio      consumo médio    consumo mínimo   consumo máximo  custo mínimo     custo máximo");
+                writer.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
                 for (int i = 0; i < tableModel2.getRowCount(); i++) {
                     for (int j = 0; j < tableModel2.getColumnCount(); j++) {
-                        writer.write(tableModel2.getValueAt(i, j).toString());
-                        if (j < tableModel2.getColumnCount() - 1) {
-                            writer.write(",");
-                        }
+                        String value = tableModel2.getValueAt(i, j).toString();
+                        writer.printf("%-17s", value);
                     }
-                    writer.newLine();
+                    writer.println();
                 }
 
                 System.out.println("Data saved successfully to the file: " + nomeArquivo);
@@ -372,6 +422,194 @@ class ComparadorContas {
                 System.out.println("Error saving table data: " + ex.getMessage());
             }
         }
+    }
+}
+
+    class CalculatorExtension extends GUI {
+    private JPanel calculatorPanel;
+    private JTextField numField1;
+    private JTextField numField2;
+    private JTextField resultField;
+    private JButton addButton;
+    private JButton subtractButton;
+    private JButton multiplyButton;
+    private JButton divideButton;
+    private JButton sqrtButton;
+    private JButton powerButton;
+
+    public CalculatorExtension() {
+        calculatorPanel = createCalculatorPanel();
+    }
+
+    public JPanel getCalculatorPanel() {
+        return calculatorPanel;
+    }
+
+    private JPanel createCalculatorPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.insets = new Insets(5, 5, 5, 5);
+
+        JLabel label1 = new JLabel("Number 1:");
+        numField1 = new JTextField(10);
+
+        JLabel label2 = new JLabel("Number 2:");
+        numField2 = new JTextField(10);
+
+        JLabel resultLabel = new JLabel("Result:");
+        resultField = new JTextField(10);
+        resultField.setEditable(false);
+
+        addButton = new JButton("+");
+        subtractButton = new JButton("-");
+        multiplyButton = new JButton("*");
+        divideButton = new JButton("/");
+        sqrtButton = new JButton("√");
+        powerButton = new JButton("^");
+
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calculateResult(Operation.ADD);
+            }
+        });
+
+        subtractButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calculateResult(Operation.SUBTRACT);
+            }
+        });
+
+        multiplyButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calculateResult(Operation.MULTIPLY);
+            }
+        });
+
+        divideButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calculateResult(Operation.DIVIDE);
+            }
+        });
+
+        sqrtButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calculateResult(Operation.SQUARE_ROOT);
+            }
+        });
+
+        powerButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                calculateResult(Operation.POWER);
+            }
+        });
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        panel.add(label1, constraints);
+
+        constraints.gridx = 1;
+        panel.add(numField1, constraints);
+
+        constraints.gridx = 2;
+        panel.add(addButton, constraints);
+
+        constraints.gridx = 3;
+        panel.add(subtractButton, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        panel.add(label2, constraints);
+
+        constraints.gridx = 1;
+        panel.add(numField2, constraints);
+
+        constraints.gridx = 2;
+        panel.add(multiplyButton, constraints);
+
+        constraints.gridx = 3;
+        panel.add(divideButton, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        panel.add(resultLabel, constraints);
+
+        constraints.gridx = 1;
+        panel.add(resultField, constraints);
+
+        constraints.gridx = 2;
+        panel.add(sqrtButton, constraints);
+
+        constraints.gridx = 3;
+        panel.add(powerButton, constraints);
+
+        return panel;
+    }
+
+        private void calculateResult(Operation operation) {
+        String input1 = numField1.getText();
+        String input2 = numField2.getText();
+        double num1 = parseDouble(input1);
+        double num2 = parseDouble(input2);
+        double result = 0;
+
+        switch (operation) {
+            case ADD:
+                result = num1 + num2;
+                break;
+            case SUBTRACT:
+                result = num1 - num2;
+                break;
+            case MULTIPLY:
+                result = num1 * num2;
+                break;
+            case DIVIDE:
+                result = num1 / num2;
+                break;
+            case SQUARE_ROOT:
+                result = Math.sqrt(num1);
+                break;
+            case POWER:
+                result = Math.pow(num1, num2);
+                break;
+        }
+
+        boolean isFloatResult = operation != Operation.SQUARE_ROOT && (isFloat(num1) || isFloat(num2));
+        String formattedResult = formatResult(result, isFloatResult, operation);
+
+        resultField.setText(formattedResult);
+    }
+
+    private String formatResult(double result, boolean isFloatResult, Operation operation) {
+        if (isFloatResult || operation == Operation.SQUARE_ROOT) {
+            return String.format("%.4f", result);
+        } else if (result % 1 == 0) {
+            return String.valueOf((int) result);
+        } else {
+            return String.valueOf(result);
+        }
+    }
+
+    private double parseDouble(String input) {
+        try {
+            return Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    private boolean isFloat(double value) {
+        return value != (int) value;
+    }
+
+    private enum Operation {
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE,
+        SQUARE_ROOT,
+        POWER
     }
 }
 
